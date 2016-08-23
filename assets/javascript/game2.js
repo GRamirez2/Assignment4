9d9bd1cf18ea81f32576774c00f67e2c21becc89
George Ramirez <george.ramirez2@gmail.com>
2016-08-22 22:49:34 -0500

    how to stop the second click

---
+++ assets/javascript/game2.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+
+
+// move Bobby to fight area
+$("#player1").on("click", function(){
+	$("#player1").appendTo("#section2");
+	$("#subSection").appendTo("#section3");
+});
+
+ // move Giada to fight area
+$("#player2").on("click", function(){
+	$("#player2").appendTo("#section2");
+	$("#subSection").appendTo("#section3");
+});
+
+// move Guy to fight area
+$("#player3").on("click", function(){
+	$("#player3").appendTo("#section2");
+	$("#subSection").appendTo("#section3");
+});
+
+// move Alton to fight area
+$("#player4").on("click", function(){
+	$("#player4").appendTo("#section2");
+	$("#subSection").appendTo("#section3");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+});
