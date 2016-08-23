dfe6da841739d2b1469d14f10db698eb9f4b4d58
George Ramirez <george.ramirez2@gmail.com>
2016-08-23 00:10:55 -0500

    small changes

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
-
+// write fuction that has a counter, when the fighters are selected they
+// need to pass info to a new id, if a value, could be their health value, 
+// total is >100 than allow another player to be selected, if not no new player
+// can be selected.(another way is to make a fuction above this all these onclicks that
+// have a var count=0 and on each click count+=1, if (count==2) then no more clicks.
+// I could this for the begining and set the second action in the fighting fuction.
 
 // move Bobby to fight area
 $("#player1").on("click", function(){
@@ -27,7 +32,14 @@ $("#player4").on("click", function(){
 });
 
 
+// Need to add more events to the move fuction, adding data (maybe)
+// and assigning the fighters new IDs.
 
+// I might be able to give a condition on the new IDs that will disable the click.
+
+$("#fightButton").on("click", function(){
+	$("#newID")does something, and other $("newID") does something
+});
 
 
 
