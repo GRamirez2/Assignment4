81c94427d912326a59d3160176f113244c2d8c87
George Ramirez <george.ramirez2@gmail.com>
2016-08-24 00:54:16 -0500

    working until function and need to sort out data

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -23,19 +23,18 @@ $(document).ready(function(){
 // 	battleZone:"t/f",
 // }
 
-// creating data values for each player, in array of life,health&strikeForce
-$("#player1").data("life", {health: 150, strikeForce: 25});
-// console.log($("#bobbyHealth").data("life").strikeForce, $("#bobbyHealth").data("life").health);
-$("#bobbyHealth").text($("#player1").data("life").health);
+// creating data values for each player, for health & strikeForce
+$("#player1").data("health",150). data("strikeForce", 25);
+$("#bobbyHealth").text($("#player1").data("health"));
 
-$("#giadaHealth").data("life", {health: 125, strikeForce: 50});
-$("#giadaHealth").text($("#giadaHealth").data("life").health);
+$("#player2").data("health", 125).data("strikeForce", 50);
+$("#giadaHealth").text($("#player2").data("health"));
 
-$("#guyHealth").data("life", {health: 100, strikeForce: 15});
-$("#guyHealth").text($("#guyHealth").data("life").health);
+$("#player3").data("health", 100).data("strikeForce", 15);
+$("#guyHealth").text($("#player3").data("health"));
 
-$("#altonHealth").data("life", {health: 100, strikeForce: 75});
-$("#altonHealth").text($("#altonHealth").data("life").health);
+$("#player4").data("health", 100).data("strikForce", 75);
+$("#altonHealth").text($("#player4").data("health"));
 
 $("#fightButton").hide();
 $("#textOnScreen2").hide();
@@ -43,15 +42,7 @@ $("#textOnScreen3").hide();
 $("#textOnScreen4").hide();
 
 var counter = 0;
-
-
-// write fuction that has a counter, when the fighters are selected they
-// need to pass info to a new id, if a value, could be their health value, 
-// total is >100 than allow another player to be selected, if not no new player
-// can be selected.(another way is to make a fuction above this all these onclicks that
-// have a var count=0 and on each click count+=1, if (count==2) then no more clicks.
-// I could this for the begining and set the second action in the fighting fuction.
-
+var player1H = 0;
 
 
 // move Bobby to fight area
@@ -62,15 +53,15 @@ $("#player1").on("click", function(){
 	$("#subSection").appendTo("#section3");
 	
 // insert playerTwoSelect funtion that will turn the second selector off and on
-	$("#player1").attr("id", "#fighter1");
-	// $("#section2").html("#fighter1");/*switched? */
-	console.log($("#bobbyHealth").data("life"));	/*This works fine*/
-	// console.log($("#fighter1").data("life").strikeForce); /*This isn't working*/
-	
-	// counter ++
-	// console.log("counter number is "+counter)
-	
+	counter ++
+	if ( counter ==2){
+		$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
+	}else{$("#section2").show();$("#fightButton").hide();$("#textOnScreen2").show();
+		};
+
+
 });
+
 
 
 
@@ -79,6 +70,10 @@ $("#player2").on("click", function(){
 	$("#player2").appendTo("#section2");
 	$("#subSection").appendTo("#section3");
 	counter ++
+	if ( counter ==2){
+		$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
+	}else{$("#section2").show();$("#fightButton").hide();$("#textOnScreen2").show();
+		};
 	// console.log(counter)/*Why can't this see the counter?*/
 
 });
@@ -87,14 +82,38 @@ $("#player2").on("click", function(){
 $("#player3").on("click", function(){
 	$("#player3").appendTo("#section2");
 	$("#subSection").appendTo("#section3");
+	counter ++
+	if ( counter ==2){
+		$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
+	}else{$("#section2").show();$("#fightButton").hide();$("#textOnScreen2").show();
+		};
 });
 
 // move Alton to fight area
 $("#player4").on("click", function(){
 	$("#player4").appendTo("#section2");
 	$("#subSection").appendTo("#section3");
+	counter ++
+	if ( counter ==2){
+		$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
+	}else{$("#section2").show();$("#fightButton").hide();$("#textOnScreen2").show();
+		};
 	
 });
+// run the math on the battle numbers from elements in parent div, if dead or win flip counter
+// $("#fightButton").on("click", function(){
+// 	($("#section2":last-child).data("health")) - ($("#section2":first-child).data("strikeForce"));
+// 	($("#section2":first-child).data("health")); - ($("#section2":last-child).data("strikeForce"));
+// 	html data health and strike force for each to textOnScreen4
+
+// 	if last child health is <= 0 add to first child strikeforce and open counter, if first child is <=0, loose game.
+// 	Toogle on #textOnScreen4 until win or loose. 
+	
+// 	create huge "you lost, want to play again" button to restart the game. 
+// };
+
+
+
 
 
 // Need to add more events to the move fuction, adding data (maybe)
@@ -108,8 +127,8 @@ $("#player4").on("click", function(){
 
 // Begining of BATTLE FUNCTION
 
-
-
+var player1H=($("#player1").data("health"));
+console.log(player1H + "plan b");
 
 
 
