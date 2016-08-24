da22bd85ae24c6e2073938160c507d0d1c09ac10
George Ramirez <george.ramirez2@gmail.com>
2016-08-24 15:00:51 -0500

    working up to big function

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,31 +1,8 @@
 $(document).ready(function(){
 
-// This idea didn't work, I trying data for each player
-// var players = {
-
-// 	player1: {health:150,strikeForce: 25},
-
-// 	player2: {health:125,strikeForce: 50},
-					
-// 	player3: {health:100,strikeForce: 15},
-// 			
-// 	player4: {health:175,strikeForce: 75},
-// 			
-// 	fighter1: {health:0,strikeForce: 0},
-
-// 	fighter2: {health:0,strikeForce: 0},
-
-// 	startGame:"",
-// 	nextPlayer:"",
-// 	battle:"",
-// 	win:"",
-// 	lose:"",
-// 	battleZone:"t/f",
-// }
-
 // creating data values for each player, for health & strikeForce and printing
-// the health to the screen
-$("#player1").data("health",150). data("strikeForce", 25);
+
+$("#player1").data("health",150).data("strikeForce", 25);
 $("#bobbyHealth").text($("#player1").data("health"));
 
 $("#player2").data("health", 125).data("strikeForce", 50);
@@ -34,79 +11,95 @@ $("#giadaHealth").text($("#player2").data("health"));
 $("#player3").data("health", 100).data("strikeForce", 15);
 $("#guyHealth").text($("#player3").data("health"));
 
-$("#player4").data("health", 100).data("strikForce", 75);
+$("#player4").data("health", 100).data("strikeForce", 75);
 $("#altonHealth").text($("#player4").data("health"));
 
-$("#fightButton").hide();
+$("#textOnScreen1").show();
 $("#textOnScreen2").hide();
 $("#textOnScreen3").hide();
 $("#textOnScreen4").hide();
+$("#fightButton").hide();
 
 var counter = 0;
-var player1H = 0;
+var win = 3;
+
 
 
 // move Bobby to fight area
 $("#player1").on("click", function(){
-
-	$("#textOnScreen1").hide();/*controling the message on screen*/
 	$("#player1").appendTo("#section2");
 	$("#subSection").appendTo("#section3");
-	
-
+	$("#textOnScreen1").hide();
 	counter ++
 	if ( counter ==2){
 		$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
-	}else{$("#section2").show();$("#fightButton").hide();$("#textOnScreen2").show();
+	}else{$("#textOnScreen2").show();$("#fightButton").hide();
 		};
+	
+});/*END of Bobby onClick*/
 
 
-});
 
 
-
-
- // move Giada to fight area
+ // move Giada to fight area, counter++, and control instructions on screen
 $("#player2").on("click", function(){
 	$("#player2").appendTo("#section2");
 	$("#subSection").appendTo("#section3");
+	$("#textOnScreen1").hide();
 	counter ++
 	if ( counter ==2){
-		$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
-	}else{$("#section2").show();$("#fightButton").hide();$("#textOnScreen2").show();
+			$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
+	}else{$("#textOnScreen2").show();$("#fightButton").hide();
 		};
 
-	console.log($("#section2:first-child").data("strikeForce"));
-	console.log($("#section2:last-child").data("health"));
-	console.log($("#player2").data("health"));
+});/*END of Giada onClick*/
 
-});
 
-// move Guy to fight area
+// move Guy to fight area,counter++, and control instructions on screen
 $("#player3").on("click", function(){
 	$("#player3").appendTo("#section2");
 	$("#subSection").appendTo("#section3");
+	$("#textOnScreen1").hide();
 	counter ++
 	if ( counter ==2){
 		$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
-	}else{$("#section2").show();$("#fightButton").hide();$("#textOnScreen2").show();
+	}else{$("#textOnScreen2").show();$("#fightButton").hide();
 		};
-});
+});/*END of Guy onClick*/
 
-// move Alton to fight area
+
+// move Alton to fight area,counter++, and control instructions on screen
 $("#player4").on("click", function(){
 	$("#player4").appendTo("#section2");
 	$("#subSection").appendTo("#section3");
+	$("#textOnScreen1").hide();
 	counter ++
 	if ( counter ==2){
-		$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
-	}else{$("#section2").show();$("#fightButton").hide();$("#textOnScreen2").show();
+			$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
+	}else{$("#textOnScreen2").show();$("#fightButton").hide();
 		};
 	
-});
-// BATTLE Button FUNCTION
+});/*END of Alton onClick*/
+
+
+// BEGINING of Battle Button Fuction,counter++
+$("#fightButton").on("click", function(){
+	console.log($("article section:first").data("strikeForce"));
+	console.log($("article section:first").data("health"));
+	console.log($("article section:last").data("strikeForce"));
+	console.log($("article section:last").data("health"));
+
+	($("article section:last").data("health"))-($("article section:first").data("strikeForce"))
+	$("article section:last").text($("article section:last").data("health"));
+
+
+
+	});
+
+
+
 // run the math on the battle numbers from elements in parent div, if dead or win flip counter
-// $("#fightButton").on("click", function(){
+
 // 	($("#section2":last-child).data("health")) - ($("#section2":first-child).data("strikeForce"));
 // 	($("#section2":first-child).data("health")); - ($("#section2":last-child).data("strikeForce"));
 // 	html data health and strike force for each to textOnScreen4
@@ -115,7 +108,7 @@ $("#player4").on("click", function(){
 // 	Toogle on #textOnScreen4 until win or loose. 
 	
 // 	create  "you lost, want to play again" button to restart the game. 
-// };
+// 
 
 
 
@@ -126,7 +119,7 @@ $("#player4").on("click", function(){
 // var player1H=($("#player1").data("health"));
 // console.log(player1H + "option b for battle function is to call a variable that has been assigned the number I need");
 
-
+	
 
 
 
