a6d4934955ca7c9c370ab70a91e7125c0c27615f
George Ramirez <george.ramirez2@gmail.com>
2016-08-26 16:29:46 -0500

    final bu tnew games aren't working

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -22,6 +22,9 @@ $(document).ready(function(){
 	$("#fightButton").hide();
 	$("#textOnScreen4").hide();
 	$("#textOnScreen5").hide();
+	// $("#textOnScreen4").on("click", loose());	
+	// the other button to restart game
+	var originalPlayerList = $("#section1").clone(true,true);
 	var counter = 0;
 	var win = 3;
 
@@ -40,8 +43,6 @@ $(document).ready(function(){
 			};
 		
 	});/*END of Bobby onClick*/
-
-
 
 
 	 // move Giada to fight area, counter++, and control instructions on screen
@@ -128,15 +129,20 @@ $(document).ready(function(){
 	function loose(){
 
 		if(($("article section:first").data('health')) <=0 ){
-			$("#section2").remove();
+			// $("#section2").remove();
 			$("#textOnScreen3").hide();
-			$("#textOnScreen2").hide();
-			$("#textOnScreen4").show();/* need to style this and link this to start game over*/
+			$("#textOnScreen1").hide();
+			// $("#textOnScreen2").hide();
+			$("#textOnScreen4").show();/* start game over link*/
 			$("#fightButton").hide();
-			$("article section:last, article sections:first").remove();
-			$("#section3").remove();
-			// a function that starts the game over on button click, empties, section 2 and 3, plus the one text bar with health numbers. 	
+			$("article section:last, article sections:first").detach();
+			$("article section:first, article sections:first").detach();
+			$("#section3").detach();
+			setTimeout(restartGame, 1000 * 3);
+
 				}
+
+
 	  }/*END of loose function*/
 
 	function nextPlayer(){
@@ -165,10 +171,9 @@ $(document).ready(function(){
 			console.log(fighter1Strike+"and"+fighter1StrikeStronger);
 			};
 				
-		wingame();
-		loose();
-	  };/*END OF NEXTPLAYER FUNCTION*/	
-
+				wingame();
+				loose();
+			  };/*END OF NEXTPLAYER FUNCTION*/	
 
 	function wingame (){
 		//Player 1 wins game 
@@ -180,13 +185,94 @@ $(document).ready(function(){
 		$("article section:last, article sections:first").remove();
 		$("#textOnScreen3").empty();
 		$("#textOnScreen2").hide();
-		// Need to make a restart game function
+		setTimeout(restartGame, 1000 * 3);
 
-		}
+			}
 
-	 };/*END of WIN FUNCTION*/	
+	 	  };/*END of WIN FUNCTION*/	
 
-	 /*end of start game*/
-	// 	create  "you lost, want to play again" button to restart the game. 
+	 //create a function that starts the game over, baasically repusing the first section to the screen and clearing all data and other divs.
+	//This will also be linked to from the buttons on the win and loose screen.
+	function restartGame(){
 
-});
+		$("#section1").replaceWith(originalPlayerList);
+
+				$("#player1").data("health",150).data("strikeForce", 30);
+				$("#bobbyHealth").text($("#player1").data("health"));
+
+				$("#player2").data("health", 155).data("strikeForce", 20);
+				$("#giadaHealth").text($("#player2").data("health"));
+
+				$("#player3").data("health", 150).data("strikeForce", 10);
+				$("#guyHealth").text($("#player3").data("health"));
+
+				$("#player4").data("health", 150).data("strikeForce", 80);
+				$("#altonHealth").text($("#player4").data("health"));
+
+				
+				
+				$("#textOnScreen1").show();
+				$("#textOnScreen2").hide();
+				$("#textOnScreen3").hide();
+				$("#textOnScreen4").hide();
+				$("#fightButton").hide();
+				$("#textOnScreen4").hide();
+				$("#textOnScreen5").hide();
+				$("#section3").show();
+
+				var counter = 0;
+				var win = 3;
+
+				
+					// move Bobby to fight area
+					$("#player1").on("click", function(){
+						$("#player1").appendTo("#section2");
+						$("#subSection").appendTo("#section3");
+						$("#textOnScreen1").hide();
+						$("#player1").off();
+						counter ++
+						if ( counter ==2){
+							$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
+						}else{$("#textOnScreen2").show();$("#fightButton").hide();
+							};
+					});/*END of Bobby onClick*/
+					 // move Giada to fight area, counter++, and control instructions on screen
+					$("#player2").on("click", function(){
+						$("#player2").appendTo("#section2");
+						$("#subSection").appendTo("#section3");
+						$("#textOnScreen1").hide();
+						$("#player2").off();
+						counter ++
+						if ( counter ==2){
+								$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
+						}else{$("#textOnScreen2").show();$("#fightButton").hide();
+							};
+					});/*END of Giada onClick*/
+					// move Guy to fight area,counter++, and control instructions on screen
+					$("#player3").on("click", function(){
+						$("#player3").appendTo("#section2");
+						$("#subSection").appendTo("#section3");
+						$("#textOnScreen1").hide();
+						$("#player3").off();
+						counter ++
+						if ( counter ==2){
+							$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
+						}else{$("#textOnScreen2").show();$("#fightButton").hide();
+							};
+					});/*END of Guy onClick*/
+					// move Alton to fight area,counter++, and control instructions on screen
+					$("#player4").on("click", function(){
+						$("#player4").appendTo("#section2");
+						$("#subSection").appendTo("#section3");
+						$("#textOnScreen1").hide();
+						$("#player4").off();
+						counter ++
+						if ( counter ==2){
+								$("#fightButton").show();$("#section3").hide();$("#textOnScreen2").hide();
+						}else{$("#textOnScreen2").show();$("#fightButton").hide();
+							};
+					});/*END of Alton onClick*/
+
+				}/*end of restart game*/
+
+});/*End of Ready Fuction*/
